93c3f95f58a6ceca5dcb497d3333eddff800b3f7
truco23 <trucopaulista23@gmail.com>
2019-06-13 19:17:19 -0300

    Estilo do componente new

--- src/new/pages/new-page/NewPage.js
+++ src/new/pages/new-page/NewPage.js
@@ -1,10 +1,52 @@
 import React, { Component } from 'react';
 
+import './new-page.css';
+
 class NewPage extends Component {
     
     render() { 
         return (  
-            <h1>New</h1>
+            <form className="form">
+                <fieldset>
+                    <input 
+                        className="form-input"
+                        type="file"
+                        name="image"
+                    />
+
+                    <input 
+                        className="form-input"
+                        type="text"
+                        name="author"
+                        placeholder="Autor do post"
+                    />
+
+                    <input 
+                        className="form-input"
+                        type="text"
+                        name="place"
+                        placeholder="Local do post"
+                    />
+
+                    <input 
+                        className="form-input"
+                        type="text"
+                        name="description"
+                        placeholder="Descrição do post"
+                    />
+                    <input 
+                        className="form-input"
+                        type="text"
+                        name="hastag"
+                        placeholder="Hastag do post"
+                    />
+                </fieldset>
+
+                <fieldset>
+                    <button className="form-button" type="submit">Cadastrat</button>
+                </fieldset>
+
+            </form>
         );
     }
 }
